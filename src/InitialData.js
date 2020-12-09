40223001f69fa51668eb54ab437f2312ba338454
SaranyaSivadasan <68867201+SaranyaSivadasan@users.noreply.github.com>
2020-12-09 13:21:46 +0530

    change

--- src/InitialData.js
+++ src/InitialData.js
@@ -1,44 +1,46 @@
-var studentArray = [{
+var studentArray = [
+  {
     id: 1,
-    name: 'Jayesh',
+    name: "Rahul",
     currentClass: 5,
-    division: 'A'
-},
-{
+    division: "A"
+  },
+  {
     id: 2,
-    name: 'Minakshi',
+    name: "Anjali",
     currentClass: 12,
-    division: 'C'
-},
-{
+    division: "C"
+  },
+  {
     id: 3,
-    name: 'Drisham',
-    currentClass: 7,
-    division: 'C'
-},
-{
-    id: 4,
-    name: 'Kamlesh',
-    currentClass: 7,
-    division: 'A'
-},
-{
-    id: 5,
-    name: 'Dhoni',
-    currentClass: 10,
-    division: 'D'
-},
-{
-    id: 6,
-    name: 'Piyush',
-    currentClass: 10,
-    division: 'A'
-},
-{
-    id: 7,
-    name: 'Aansh',
+    name: "Akhila",
     currentClass: 8,
-    division: 'A'
-}]
+    division: "C"
+  },
+  {
+    id: 4,
+    name: "Dayal",
+    currentClass: 7,
+    division: "A"
+  },
+  {
+    id: 5,
+    name: "Rishi",
+    currentClass: 9,
+    division: "D"
+  },
+  {
+    id: 6,
+    name: "Blessy",
+    currentClass: 10,
+    division: "A"
+  },
+  {
+    id: 7,
+    name: "Farzy",
+    currentClass: 11,
+    division: "A"
+  }
+];
 
 module.exports = studentArray;
